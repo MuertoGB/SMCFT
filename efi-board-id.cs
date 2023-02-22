8d7a61265af49ab21e44e9d02c52150ec8d0f24b
David R <114369357+MuertoGB@users.noreply.github.com>
2023-02-22 04:17:34 +0000

    Create efi-board-id.cs

---
+++ efi-board-id.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+
+namespace MacTools.EFI
+{
+    class ROMHelper
+    {
+        public static string boardIdFromEfiROM(string FilePath)
+        {
+            using (FileStream fileStream = new FileStream(FilePath, FileMode.Open))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(fileStream))
+                {
+                    long len = fileStream.Length;
+                    byte[] bIdBytes = new byte[12];
+                    binaryReader.BaseStream.Position = 0x1060;
+                    bIdBytes = binaryReader.ReadBytes(0x12);
+
+                    // Looking for U+7C000019
+                    if (bIdBytes[0] == 0x7C && bIdBytes[3] == 0x19)
+                    {
+                        // Found magic bytes, return the board-id.
+                        return string.Concat("Mac-", BitConverter.ToString(bIdBytes, 4, 8).Replace("-", ""));
+                    }
+                }
+            }
+            return "Magic not found, better luck next time.";
+        }
+    }
+}
